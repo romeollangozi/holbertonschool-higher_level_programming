7fe4522436f297a769712e7e74a29ae3396ee0fa
romeollangozi <romeollangozi123@gmail.com>
2023-10-02 11:01:51 -0700

    s

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for number in reversed(my_list):
+    new_list = my_list[::-1]
+    for number in new_list:
         print("{:d}".format(number))
