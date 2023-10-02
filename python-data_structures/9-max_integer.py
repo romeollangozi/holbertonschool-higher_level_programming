cac0463d571ff047b95c762ec6746428a1d947c1
romeollangozi <romeollangozi123@gmail.com>
2023-10-02 12:35:56 -0700

    s

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max_number = 0
     if len(my_list) == 0:
         return None
+    max_number = my_list[0]
     for number in my_list:
         if number > max_number:
             max_number = number
