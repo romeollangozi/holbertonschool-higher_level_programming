d12778af3486d052d1a57c6e2971109914fad2c4
romeollangozi <romeollangozi123@gmail.com>
2023-10-03 05:10:27 -0700

    s

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary != None or key != None or value != None:
+    if a_dictionary or key or value is not None:
         a_dictionary[key] = value
         return a_dictionary
