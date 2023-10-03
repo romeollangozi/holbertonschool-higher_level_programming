4aeff5191bea72377c671025319cbc68d1de2015
romeollangozi <romeollangozi123@gmail.com>
2023-10-03 14:27:40 -0700

    s

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        max_val = max(a_dictionary, default=None)
+        max_val = max(a_dictionary, key=a_dictionary.get, default=None)
         return max_val
     else:
         return None
