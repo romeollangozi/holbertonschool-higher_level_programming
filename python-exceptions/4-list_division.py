4e0a13eee874037fbcdf1798bad2f15cb594894c
romeollangozi <romeollangozi123@gmail.com>
2023-10-05 01:17:08 -0700

    s

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("division by zero")
+            print("division by 0")
         except TypeError:
             print("wrong type")
         except IndexError:
