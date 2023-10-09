57956781e099227d9fd22a3c94dc7fee729f8fb5
romeollangozi <romeollangozi123@gmail.com>
2023-10-09 08:17:29 -0700

    s

---
+++ python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+
+Simple python file for createing a Square class
+and its initiliazation with one private instance attribute
+'''
+
+
+class Square:
+    '''
+
+    Square class
+    '''
+
+    def __init__(self, size=0):
+        '''
+
+        Init method for square instances
+        Args:
+            size (int): Size of the square
+        '''
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
