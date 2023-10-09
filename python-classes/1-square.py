d1941f3ba6afdb71417348230b1f8d1df268846d
romeollangozi <romeollangozi123@gmail.com>
2023-10-09 07:47:46 -0700

    s

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 '''
-Simple class definitaion in Python
+Simple python file for createing a Square class
+and its initiliazation with one private instance attribute
 '''
 
 
 class Square:
+    '''
+    Square class
+    '''
+
     def __init__(self, size):
         '''
         Init method for square instances
         Args:
             size (int): Size of the square
         '''
+
         self.__size = size
