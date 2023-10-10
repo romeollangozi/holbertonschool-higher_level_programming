2b19d2f4bc01c1bfd68f27e4dd437def264a635f
romeollangozi <romeollangozi123@gmail.com>
2023-10-10 01:20:49 -0700

    s

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     n_elem = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             n_elem += 1
         except IndexError:
             break
