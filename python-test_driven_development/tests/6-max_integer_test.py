bb3ac434d5de11062d25291f3682dd41ec101ef0
romeollangozi <romeollangozi123@gmail.com>
2023-10-12 03:11:20 -0700

    s

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -12,10 +12,13 @@ class MaxIntegerTest(unittest.TestCase):
     '''
     def test_max_integer(self):
         '''
-        Test method for checking correct output for
-        given values
+        Test method for checking correct output
         '''
 
         self.assertAlmostEqual(max_integer([1, 2, 3]), 3)
         self.assertAlmostEqual(max_integer([5, 7, 9]), 9)
         self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([-1, -6, -5, -4]), -1)
+        self.assertAlmostEqual(max_integer([-1, 3, 2, -4]), 3)
+        self.assertAlmostEqual(max_integer([1, 5, 2.3, 5.7, 6.5]), 6.5)
+        self.assertAlmostEqual(max_integer(), None)
