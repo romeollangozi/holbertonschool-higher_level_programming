9c3be169e3b035ac707754f263bbf11d51137361
romeollangozi <romeollangozi123@gmail.com>
2023-10-12 09:21:37 -0700

    s

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+    Simple module for creating a
+    Rectnagle class with instance methods
+    and class methods
+'''
+
+
+class Recatangle:
+    '''
+        Rectangle class
+    '''
+    pass
