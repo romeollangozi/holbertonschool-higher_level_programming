9b68ebdeb4946a12b9068f936b48aa5d53f2f56b
romeollangozi <romeollangozi123@gmail.com>
2023-10-17 04:49:20 -0700

    s

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+    Python simple module
+    for demonstarative purpose
+'''
+
+
+def lookup(obj):
+    '''
+        Function that prints all methods an properties
+        of a given object
+    '''
+
+    return dir(obj)
