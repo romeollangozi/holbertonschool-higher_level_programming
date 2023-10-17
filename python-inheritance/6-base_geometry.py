cd8272114a7cacf6323820baefd8453697172450
romeollangozi <romeollangozi123@gmail.com>
2023-10-17 05:35:37 -0700

    s

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''
+Python simple module
+for demonstarative purpose
+'''
+
+
+class BaseGeometry():
+    '''
+    Class that inherits from list class
+    '''
+    def area(self):
+        '''
+        Instance method to raise an Exception
+        '''
+        raise Exception("area() is not implemented")
