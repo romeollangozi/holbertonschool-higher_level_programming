06a22b7525d7cb58655cdc5135abe2f1251413ae
romeollangozi <romeollangozi123@gmail.com>
2023-10-18 01:12:15 -0700

    s

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Python simple module
+for demonstarative purpose
+'''
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    '''
+    Class that inherits from list class
+    '''
+    def __init__(self, size):
+        '''
+        Init function for Square class
+        '''
+        Rectangle.integer_validator(self, "size", size)
+        Rectangle.__init__(self, size, size)
+        self.__size = size
+
+    def area(self):
+        '''
+        Instance method to calculate area
+        '''
+        return self.__size ** 2
