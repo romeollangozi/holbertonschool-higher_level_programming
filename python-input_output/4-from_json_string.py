a8d4880c00bb2a50bec5c9b0b706ab9c280a04d9
romeollangozi <romeollangozi123@gmail.com>
2023-10-18 11:31:27 -0700

    s

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Module for demostrative purposes
+'''
+
+
+import json
+
+
+def from_json_string(my_str):
+    '''
+    Function to convert an object
+    as a json string
+    '''
+
+    return json.loads(my_str)
