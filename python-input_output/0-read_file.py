e9b65d003c853e64e3286a7e55830a47d60285bc
romeollangozi <romeollangozi123@gmail.com>
2023-10-18 10:45:13 -0700

    s

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+Python module for demostrative purpose
+'''
+
+
+def read_file(filename=""):
+    '''
+    Simple function for reading specified file
+    '''
+
+    with open(filename, 'r', encoding="utf-8") as my_file:
+        print(my_file.read())
