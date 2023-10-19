411d843f554f6c814cf0830c49baebd8eee39847
romeollangozi <romeollangozi123@gmail.com>
2023-10-19 01:38:17 -0700

    s

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+'''
+Module for demostrative purposes
+'''
+
+
 from sys import argv
 from os.path import exists
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-
-'''
-Module for demostrative purposes
-'''
 
 
 my_list = []
