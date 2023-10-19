f3eaed5211685dbe8cf7d8d2745e00427521c264
romeollangozi <romeollangozi123@gmail.com>
2023-10-19 02:57:20 -0700

    s

---
+++ python-input_output/11-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''
+Module for demostrative purposes
+'''
+
+
+class Student:
+    '''
+    Simple class Student
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+        Initialization method for Stduent objects
+        '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        Function to returns a dictionary
+        represantation of Student object
+        '''
+        if type(attrs) is list and all(type(thing) is str for thing in attrs):
+            new_dict = {}
+            for attribute in attrs:
+                if attribute in self.__dict__.keys():
+                    new_dict[attribute] = self.__dict__[attribute]
+            return new_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        '''
+        Function that replaces all attributes
+        of the Student instance
+        '''
+
+        json_keys = json.keys()
+        for key in json_keys:
+            if key in self.__dict__.keys():
+                self.__dict__[key] = json[key]
