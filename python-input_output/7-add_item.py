13f3ebcc78b57256fff1568723a5f3ac80485bf6
romeollangozi <romeollangozi123@gmail.com>
2023-10-19 01:35:20 -0700

    s

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+from os.path import exists
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+'''
+Module for demostrative purposes
+'''
+
+
+my_list = []
+if exists("add_item.json"):
+    my_list = load_from_json_file("add_item.json")
+for args in argv[1:]:
+    my_list.append(args)
+save_to_json_file(my_list, "add_item.json")
