6f5c6f5da3ac2e06523520e8d1ddb3d28c6e56d8
romeollangozi <romeollangozi123@gmail.com>
2023-10-23 05:20:59 -0700

    s

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ Module for demostrattive purposes
 '''
 
 
+import json
+
+
 class Base:
     '''
     Class for Base objects
@@ -17,3 +20,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        '''
+         Returns the JSON string representation
+         of list_dictionaries
+        '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
