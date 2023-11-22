d153f0ccd8ca42fc72cab6477f28ff97e0cef01e
romeollangozi <romeollangozi123@gmail.com>
2023-11-22 10:59:27 +0100

    s

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- task 13
+SELECT gen.name AS genre, COUNT(*) as number_of_shows FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id=tv_shows.id
+JOIN tv_genres AS gen
+ON gen.id=tv_show_genres.genre_id
+GROUP BY gen.name
+ORDER BY number_of_shows DESC;
