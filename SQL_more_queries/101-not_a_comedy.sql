0cfdce7ab9fa7977a14ac9c22140c3f10053b70e
romeollangozi <romeollangozi123@gmail.com>
2023-11-22 16:51:34 +0100

    s

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,5 @@
 -- all genres
-WITH tmp_table AS ((SELECT tv_shows.title FROM tv_show_genres
+(SELECT tv_shows.title FROM tv_show_genres
 JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title ASC)
@@ -8,6 +8,5 @@ EXCEPT
 JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_genres.name='COMEDY'
-ORDER BY tv_shows.title ASC))
-SELECT tmp_table.title FROM tmp_table
-ORDER BY tmp_table.title;
+ORDER BY tv_shows.title ASC)
+ORDER BY 1;
