fe362d620b424e0a896bb4a2c361e1605a08fb2c
romeollangozi <romeollangozi123@gmail.com>
2023-11-22 11:17:01 +0100

    s

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- comment
+SELECT tv_shows.title, tv_genres.name  FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_show_genres.show_id=tv_shows.id
+LEFT OUTER JOIN tv_genres
+ON tv_genres.id=tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
