29128751ce00aafc86de479274a5033d243dd068
romeollangozi <romeollangozi123@gmail.com>
2023-11-28 16:37:59 +0100

    s

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Module to create City model class
+to be used as an Object mapper
+'''
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    '''
+    State base model
+    '''
+
+    __tablename__ = "cities"
+
+    id = Column(Integer,
+                autoincrement=True,
+                nullable=False,
+                unique=True,
+                primary_key=True)
+
+    name = Column(String(128),
+                  nullable=False)
+
+    state_id = Column(Integer,
+                      ForeignKey('states.id')
+                      ,nullable=False)
