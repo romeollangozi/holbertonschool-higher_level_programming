1afddb6ddaad62c34090db2140f158ccaf72abb6
romeollangozi <romeollangozi123@gmail.com>
2023-11-28 14:52:56 +0100

    s

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,30 +5,25 @@ to be used as an Object mapper
 '''
 
 
-from sqlalchemy import declarative_base,\
-      Column, Integer, String, create_engine
-from sys import argv
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
-if __name__ == '__main__':
-    engine = create_engine(f'mysql+mysqldb://\
-                        {argv[1]}:{argv[2]}\
-                        :3306@localhost')
-    Base = declarative_base()
 
-    class State(Base):
-        '''
-        State base model
-        '''
+Base = declarative_base()
 
-        __tablename__ = "states"
 
-        id = Column(Integer,
-                    autoincrement=True,
-                    nullable=False,
-                    unique=True,
-                    primary_key=True)
+class State(Base):
+    '''
+    State base model
+    '''
 
-        name = Column(String(128),
-                      nullable=False)
+    __tablename__ = "states"
 
-    Base.metadata.create_all(engine)
+    id = Column(Integer,
+                autoincrement=True,
+                nullable=False,
+                unique=True,
+                primary_key=True)
+
+    name = Column(String(128),
+                  nullable=False)
