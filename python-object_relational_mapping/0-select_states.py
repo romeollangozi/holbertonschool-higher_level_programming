00053ec3d69d13b035d00bddd944ea25510b93aa
romeollangozi <romeollangozi123@gmail.com>
2023-11-28 13:25:23 +0100

    s

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
+"""
+List all states from the database hbtn_0e_0_usa
+"""
+
+
 import MySQLdb
 from sys import argv
-
-
-'''
-Simple module to make a connect to mysql server
-with MySQLdb library
-'''
 
 
 if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM states ORDER BY states.id')
+    cursor.execute('SELECT * FROM states ORDER BY id ASC')
     all_rows = cursor.fetchall()
 
     for row in all_rows:
